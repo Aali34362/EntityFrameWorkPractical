f3f415ee8a5cff03f39ae862613d76f24476bb27
Aamir Ali <aa882755@gmail.com>
2024-05-15 22:46:00 +0530

    View

---
+++ EntityFrameWorkCore.Data/Migrations/20240515171132_teamleagueview.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameWorkCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class teamleagueview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE VIEW vw_TeamsAndLeagues
+                AS
+                SELECT t.Name, l.Name AS LeagueName
+                From Teams As t
+                LEFT JOIN Leagues AS l on t.LeagueId = l.Id
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP VIEW vw_TeamsAndLeagues
+            ");
+        }
+    }
+}
